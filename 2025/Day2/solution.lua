94cfefbdfe9973b460483d8790089c93dfd7452c
Unity <k.jelic.74@gmail.com>
2025-12-02 20:19:14 +0100

    Finished day 2

--- 2025/Day2/solution.lua
+++ 2025/Day2/solution.lua
@@ -30,8 +30,55 @@ function throughInterval(min,max)
 	return res
 end
 
-res = 0
-for _,j in pairs(lines) do
-	res = res + throughInterval(j[1],j[2])
+function part1()
+	res = 0
+	for _,j in pairs(lines) do
+		res = res + throughInterval(j[1],j[2])
+	end
+	return res
 end
-print(res)
+
+
+--
+--- Part 2
+--
+
+function generateSplit(n, size)
+	local split = string.sub(n, 1, size)
+	local nb = ""
+	while #nb < #n do
+		nb = nb..split
+	end
+	return nb
+end
+
+function checkPattern(n)
+	local nb = tostring(n)
+	local halflen = math.floor(#nb/2)
+	for i=1,halflen do
+		if nb == generateSplit(nb, i) then
+			return true
+		end
+	end
+	return false
+end
+
+function throughInterval2(min,max)
+	local res = 0
+	for i = min,max do
+		if checkPattern(i) then
+			res = res + i
+		end
+	end
+	return res
+end
+
+function part2()
+	res = 0
+	for _,j in pairs(lines) do
+		res = res + throughInterval2(j[1],j[2])
+	end
+	return res
+end
+
+print(part2())
