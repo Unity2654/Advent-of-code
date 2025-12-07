026f9c556652628814714c8f05d21c3189fcad59
Unity <k.jelic.74@gmail.com>
2025-12-07 18:36:50 +0100

    Solved Day 7

--- 2025/Day7/solution.lua
+++ 2025/Day7/solution.lua
@@ -1,10 +1,9 @@
+require("PriorityQueue")
 -- Parsing the input
 
-f = io.open("smallPuzzle.txt")
+f = io.open("puzzle.txt")
 source = {}
 grid = {}
-
-
 
 line = f:read("*line")
 while line do
@@ -12,7 +11,7 @@ while line do
     x = 1
     for c in line:gmatch(".") do
         if c == "S" then
-            source = {x,1}
+            source = {1,x}
         end
         x = x + 1
         table.insert(l,c)
@@ -30,33 +29,51 @@ function serialize(vector)
     return vector[1].."_"..vector[2]
 end
 
+Q = PriorityQueue.new(comparator)
+function beam(start)
+    local splits = 0
+    Q:Add(start, -start[1])
+    while Q:Size() > 0 do
+        local coords = Q:Pop()
+        if grid[coords[1]] and grid[coords[1]][coords[2]] and grid[coords[1]][coords[2]] ~= "|" then
+            while grid[coords[1]] and grid[coords[1]][coords[2]] do
+                if grid[coords[1]][coords[2]] == "^" then
+                    splits = splits + 1
+                    Q:Add({coords[1],coords[2]+1}, -coords[1])
+                    Q:Add({coords[1],coords[2]-1}, -coords[1])
+                    break;
+                end
+                grid[coords[1]][coords[2]] = "|"
+                coords[1] = coords[1] + 1
+            end
+        end
+    end
+    return splits
+end
+
+--
+--- Part 2
+--
+
 cache = {}
 
-function beam(start)
+function timelines(start)
     local coords = start
-    if cache[serialize(start)] then
-        return cache[serialize(start)]
+    local startingcoords = {start[1],start[2]}
+    if cache[serialize(startingcoords)] then
+        return cache[serialize(startingcoords)]
     end
-    while grid[coords[2]] and grid[coords[2]][coords[1]] do
-        print(coords[2], coords[1], grid[coords[2]][coords[1]])
-        if grid[coords[2]][coords[1]] == "^" then
-            local value = 1 + beam({coords[1]+1, coords[2]}) + beam({coords[1]-1, coords[2]})
-            cache[serialize(start)] = value
+    while grid[coords[1]] and grid[coords[1]][coords[2]] do
+        if grid[coords[1]][coords[2]] == "^" then
+            local value = timelines({coords[1], coords[2]+1}) + timelines({coords[1], coords[2]-1})
+            cache[serialize(startingcoords)] = value
             return value
         end
-        grid[coords[2]][coords[1]] = "#"
-        coords[2] = coords[2] + 1
+        grid[coords[1]][coords[2]] = "|"
+        coords[1] = coords[1] + 1
     end
-    cache[serialize(start)] = 0
-    return 0
+    cache[serialize(startingcoords)] = 1
+    return 1
 end
 
-print(beam(source))
-
-for _,e in pairs(grid) do
-    for _,i in pairs(e) do
-        io.write(i)
-    end
-    print()
-end
-
+print(timelines(source))
