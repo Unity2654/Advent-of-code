eb8c4141d96fbb235ad2b85d26b59f1974992330
Unity <k.jelic.74@gmail.com>
2025-12-10 19:49:06 +0100

    Tried to solve Day 9

--- 2025/Day9/solution.lua
+++ 2025/Day9/solution.lua
@@ -38,82 +38,82 @@ end
 --- Part 2
 --
 
-function isVertical(head, tail)
-    return head[2] ~= tail[2]
-end
-
-function generateEdges(c1, c2)
-    local edges = {}
-    local corner1 = {c1[1], c1[2]}
-    local corner2 = {c2[1], c2[2]}
-    local corner3 = {c2[1], c1[2]}
-    local cornet4 = {c1[1], c2[2]}
-    table.insert(edges, {corner1, corner3})
-    table.insert(edges, {corner3, corner2})
-    table.insert(edges, {corner2, cornet4})
-    table.insert(edges, {cornet4, corner1})
-    return edges
-end
-
-function isInInterval(number, b1, b2)
-    --if b1 > b2 then return number <= b1 and number >= b2 end
-    return number <= b2 and number >= b1
-end
-
-function isCrossing(e1, e2)
-    if isVertical(e1[1], e1[2]) == isVertical(e2[1], e2[2]) then return false end
-    --[[if e1[1] == e2[1] and e1[2] == e2[2] then return false end
-    if isVertical(e1[1], e1[2]) and isVertical(e2[1], e2[2]) then
-        return isInInterval(e1[1][2], e2[1][2], e2[2][2]) and isInInterval(e1[2][2], e2[1][2], e2[2][2])
+function indexof(table, number)
+    for i,e in pairs(table) do
+        if number == e then return i end
     end
-    if (not isVertical(e1[1], e1[2])) and (not isVertical(e2[1], e2[1])) then
-        return isInInterval(e1[1][1], e2[1][1], e2[2][1]) and isInInterval(e1[2][1], e2[1][1], e2[2][1])
-    end]]--
-    if isVertical(e1[1], e1[2]) then
-        return isInInterval(e2[1][2], e1[1][2], e1[2][2]) and isInInterval(e1[1][1], e2[1][1], e2[2][1])
+    return nil
+end
+
+--- Distance compression
+
+-- sorting the edges coordinates
+xAxis = {}
+yAxis = {}
+
+for _,c in pairs(corners) do
+    table.insert(xAxis, c[1])
+    table.insert(yAxis, c[2])
+end
+
+table.sort(xAxis)
+table.sort(yAxis)
+maxX = xAxis[#xAxis]
+maxY = yAxis[#yAxis]
+
+
+-- Reducing the grid
+redCorners = {}
+
+for _,c in pairs(corners) do
+    table.insert(redCorners,{indexof(xAxis,c[1]), indexof(yAxis,c[2])})
+end
+
+-- Generating the grid
+
+grid = {}
+for i=1,maxY do
+    grid[i] = {}
+    for j=1,maxX do
+        grid[i][j] = '.'
     end
-    return isInInterval(e1[1][2], e2[1][2], e2[2][2]) and isInInterval(e2[1][1], e1[1][1], e1[2][1])
 end
 
-function printEdge(e)
-    print(e[1][1], e[1][2], e[2][1], e[2][2])
+
+for _,c in pairs(redCorners) do
+    x = c[1]
+    y = c[2]
+    grid[y][x] = "#"
 end
 
-function isInRectangePoly(c1, c2)
-    edges = generateEdges(c1,c2)
-    for _,e in pairs(edges) do
-        for _,e1 in pairs(allEdges) do
-            if isCrossing(e, e1) then return false end
+for i,v in pairs(redCorners) do
+    for j = (i+1),(#redCorners+1) do
+        if j > #redCorners then
+            j = 1
         end
-    end
-    return true
-end
-
--- sorting the edges
-allEdges = {}
-
-for i,e in pairs(corners) do
-    j = i+1
-    if j > #corners then j = 1 end
-    table.insert(allEdges, {e, corners[j]})
-end
-
--- Computing the result
-function maxAreaWithinPoly()
-    local max = 0
-    for i,v in pairs(corners) do
-        for j=(i+1),#corners do
-            local v2 = corners[j]
-            local area = calcArea(v,v2)
-            if isInRectangePoly(v,v2) then
-                printVector(v)
-                printVector(v2)
-                print()
-                max = area
+        v1 = redCorners[j]
+        if v1[1] == v[1] then
+            min = math.min(v1[2],v[2])
+            max = math.max(v1[2],v[2])
+            for k=min,max do
+                grid[k][v1[1]] = "#"
             end
+        else
+            min = math.min(v1[1],v[1])
+            max = math.max(v1[1],v[1])
+            print(min,max)
+            for k=min,max do
+                io.write("("..v1[2].." " ..k..") ")
+                grid[v1[2]][k] = "#"
+            end
+            print()
         end
     end
-    return max
 end
 
-print(maxAreaWithinPoly())
+for _,l in pairs(grid) do
+    for _,e in pairs(l) do
+        io.write(e)
+    end
+    print()
+end
